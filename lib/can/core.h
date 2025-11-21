772fc68a34df2c29d98caae235ee7fdac9d16581
smoon0215 <moo19034@byui.edu>
2025-11-20 18:59:21 -0700

    alter decode func

--- lib/can/core.h
+++ lib/can/core.h
@@ -35,19 +35,20 @@ Frame encode(uint32_t id, const T& message) {
  * @returns The decoded message of type T.
  */ 
 template<typename T>
-T decode(const Frame& frame) {
-    T message;
-    uint8_t* msg_ptr = (uint8_t*) &message;
-    msg_ptr[0] = frame.data[0];
-    msg_ptr[1] = frame.data[1];
-    msg_ptr[2] = frame.data[2];
-    msg_ptr[3] = frame.data[3];
-    msg_ptr[4] = frame.data[4];
-    msg_ptr[5] = frame.data[5];
-    msg_ptr[6] = frame.data[6];
-    msg_ptr[7] = frame.data[7];
+Message<T> decode(const Frame& frame) {
+    Message<T> message;
+    // todo ensure that frame data length code is 8
+    // error handling
+    message.data = (const T*) &frame.data;
+    message.id = frame.identifier;
     return message;
 }
+
+template<typename T>
+struct Message {
+    uint32_t id;
+    T data;
+};
 
 /*
  * Manager class for handling CAN operations.
