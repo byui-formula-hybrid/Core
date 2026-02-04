684543a48d3bcf00a212453ba7b3548349913c8e
Hibyehello <36666883+Hibyehello@users.noreply.github.com>
2026-02-03 18:20:23 -0700

    Create Inverter Heartbeat controller

---
+++ lib/inverter/DTIX50/heartbeat.cpp
@@ -0,0 +1,79 @@
+#include "heartbeat.h"
+
+using namespace CAN;
+
+namespace Inverter {
+namespace DTIX50 {
+
+Controller::Controller(std::shared_ptr<Service> canService, std::unique_ptr<Core::iLockStrategy> lock_strategy, std::unique_ptr<Core::iThreadStrategy> thread_strategy) {
+    m_canService = canService;
+    m_shouldStop_mut = std::move(lock_strategy);
+    m_thread = std::move(thread_strategy);
+
+    m_shouldStop = false;
+
+    enable = { 0x01, 0xFFFFFFFFFFFFFF };
+    disable = { 0x00, 0xFFFFFFFFFFFFFF };
+
+    m_thread->setup("inverter.DTIX50.heartbeat", // name
+                    0x17U, // priority - osPriorityBelowNormal7
+                    0x01U  // attributes - osThreadJoinable
+                   );
+}
+
+void Controller::start() {
+    if(m_started) return;
+
+    // Send drive enable
+    Frame frame(0x0C52, &enable);
+
+    m_canService->transmit(&frame, 1000);
+
+    // Start the heartbeat for drive enable
+    m_thread->create(Controller::heartbeat, this);
+
+    m_started = true;
+}
+
+void Controller::stop() {
+    // Set shouldStop so that the heartbeat knows that we're stopping
+    m_shouldStop_mut->lock();
+    m_shouldStop = true;
+    m_shouldStop_mut->unlock();
+
+    // Send drive disable
+    Frame frame(0x0C52, &disable);
+
+    // Wait for the heartbeat to actually stop before sending drive disable
+    m_thread->join();
+
+    m_canService->transmit(&frame, 1000);
+
+    m_started = false;
+}
+
+// Sends a drive enable every ~250 milliseconds so the car doesn't stop
+void Controller::heartbeat(void* s) {
+    Controller* self = (Controller*)s;
+    for(;;) {
+        self->m_thread->sleep(250U);
+
+        // Check if it's time to stop
+        // TODO: Write a timeout
+        self->m_shouldStop_mut->lock();
+        if(self->m_shouldStop) 
+        {
+            self->m_shouldStop_mut->unlock();
+            return;
+        }
+        self->m_shouldStop_mut->unlock();
+
+        // Send drive enable
+        Frame frame(0x0C52, &self->enable);
+
+        self->m_canService->transmit(&frame, 1000);
+    }
+}
+
+}
+}
