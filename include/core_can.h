6fb624d8d0e7a36734e4d4241dd2fc0c7a8b4c17
Hibyehello <36666883+Hibyehello@users.noreply.github.com>
2026-06-06 21:23:15 -0600

    Remove #define conflict

--- include/core_can.h
+++ include/core_can.h
@@ -1,6 +1,6 @@
 // This is the umbrella header for the CAN library. It includes all the necessary headers for using the CAN library.
-#ifndef CAN_H
-#define CAN_H
+#ifndef CORE_CAN_H
+#define CORE_CAN_H
 
 // NOTE: Due to the inclusion of multiple lib folders in hardware, these need to be relative
 #include "../lib/can/dispatcher.h"
